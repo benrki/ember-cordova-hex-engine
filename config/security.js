39e0bc31d030529816875787d1a0d9af7ac086f4
Benjamin Ki <benjaminrki@gmail.com>
2015-12-28 22:04:37 +1100

    Add ember paper

--- config/security.js
+++ config/security.js
@@ -1,7 +1,7 @@
 module.exports = {
   'default-src': "'none'",
   'script-src': "'self'",
-  'font-src': "'self'",
+  'font-src': "'self' https://fonts.gstatic.com",
   'connect-src': "'self'",
   'img-src': "'self'",
   'style-src': "'self' 'unsafe-inline'",
