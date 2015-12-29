fba713e0514d56e23053118b2c0af4cbdf321765
Benjamin Ki <benjaminrki@gmail.com>
2015-12-29 23:17:25 +1100

    Add global async

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,8 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  // Globals
+  app.import('bower_components/async/dist/async.js');
+
   return app.toTree();
 };
--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+nvm use
+
 ember cordova plugin add cordova-plugin-file@4.0.0
